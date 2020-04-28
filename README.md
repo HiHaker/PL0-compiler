69434449e7c00ff120b4635b9387f32458511f6a
JianlongHu <1434938035@qq.com>
2020-04-28 21:34:08 +0800

    FIRST集,FOLLOW集,SELECT集以及LL1文法判别器

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 编译原理课程的大作业，实现 pl0 的词法分析程序，语法分析程序，语义分析程序。
 
+git地址为：https://github.com/HiHaker/PL0-compiler
+
 ### 词法分析程序的编写
 
 #### 设计内容
@@ -156,6 +158,8 @@ keyWords.put(":", 30);
 - 首字符为数字：判断是否全为数字，如果是，就是常数，否则就是非法单词。
 - 首字符为其他：使用switch语句判断。
 
+这里函数的参数为待分析代码的字符数组，分析的开始位置，`Token`为自定义的类，有`String`类型的值和`int`类型的值两个属性，标识识别出单词的值和种别码。
+
 ```java
 // 识别单词符号
     public Token scanner(char[] code, int begin){
@@ -246,3 +250,355 @@ keyWords.put(":", 30);
 ```
 
 至此，词法分析器已经完成。
+
+完整代码见：`LexicalAnalysis.java`文件。
+
+### LL1文法的判定
+
+需要编写一个程序，判定一个给定的文法是不是LL1文法的，重点在于实现*文法的机内表示*以及*判定算法*。
+
+关于文法的机内表示，终结符号集和非终结符号集可以用`Set<String>`来表示，为什么不使用 `Character`类呢？，主要考虑到一些符号是形如`E'`这样的，产生试集合可以使用一种数据结构，**映射**，一个左部对应一个右部列表，`Map<String, String[]>`。
+
+首先定义好一些成员变量：
+
+```java
+// 存储文法的映射（产生式规则集合）
+private Map<String, String[]> P;
+// 文法的开始符号
+private String start;
+// 非终结符号集
+private Set<String> nonEndChars;
+// 终结符号集
+private Set<String> endChars;
+
+// FIRST集合
+private Map<String, Set<String>> FIRST;
+// FOLLOW集合
+private Map<String, Set<String>> FOLLOW;
+// SELECT集合
+private Map<String, Set<String>> SELECT;
+```
+
+读取文法并且存储入映射：
+
+```java
+    // 存储文法
+private void storeGrammar(String[] Vn, String[] Vt, String[] P, String start){
+
+        this.nonEndChars = new HashSet<>();
+        this.endChars = new HashSet<>();
+        this.start = start;
+        this.P = new HashMap<>();
+
+        try{
+            // 存储产生式
+            for (String value : P) {
+                // 分割左部、右部
+                String[] split1 = value.split("->");
+                // 分割右部的不同产生式
+                String[] split2 = split1[1].split("\\|");
+                this.P.put(split1[0], split2);
+            }
+
+            // 存储符号集
+            Collections.addAll(nonEndChars, Vn);
+            Collections.addAll(endChars, Vt);
+        } catch (Exception e){
+            System.out.println("输入文法有错误!");
+            e.printStackTrace();
+        }
+    }
+```
+
+#### 计算FIRST集合
+
+对于`FIRST(x)`集合，按照以下的处理方式计算：
+
+- 如果x是终结符，`FIRST(x)`为`{x}`。
+- 如果x是空串，`FIRST(x)`为`{空串}`。
+- 如果x是非终结符，`x->Y1Y2Y3...`，逐步地从Y1计算起，如果Y1的FIRST集包含空串，将`FIRST(Y1)-空串`加入到`FIRST(X)`，继续计算`FIRST(Y2)`，... ，如果计算到某个符号`Yn`不包含空串，就结束计算。如果计算到最后一个符号也包含空串，也就是说`Y1Y2Y3...->空串`，则加入最后一个符号的FIRST集。
+
+从以上处理方式我们看出，需要递归地进行处理，先写一个方法，递归的求出某符号的FIRST集：
+
+```java
+// 递归地计算某符号的FIRST
+    private Set<String> FIRSTx(String x){
+
+        Set<String> first = new HashSet<>();
+
+        // 如果x是终结符，FIRST集就是它本身
+        // 这里，空串也包含在终结符号里
+        if (endChars.contains(x)){
+            first.add(x);
+            return first;
+        } else {
+            // 否则，就等于其右部各个符号的FIRST集相加
+            String[] rightItems = P.get(x);
+            // 遍历每一个右部
+            for (String value : rightItems){
+                List<String> cList = disassemble(value);
+
+                // 如果右部的产生式是单个符号的，就直接加入到first集合
+                if (cList.size() == 1){
+                    first.addAll(FIRSTx(cList.get(0)));
+                } else {
+                    // 如果右部是多个符号，就看串首符号
+                    for (int i=0; i<cList.size(); i++){
+                        String character = cList.get(i);
+                        // 如果是终结符，直接把它加入first集合
+                        if (endChars.contains(character)){
+                            first.add(character);
+                            break;
+                        } else {
+                            // 如果是非终结符，就计算它的FIRST集
+                            Set<String> cFirst = FIRSTx(character);
+                            // 如果包含有空串，就要继续计算后一个符号的FIRST集
+                            if (cFirst.contains("NULL")){
+                                // 如果此时是最后一个符号了
+                                // 就要把空串页加入进first集合
+                                if (i == cList.size() - 1){
+                                    first.addAll(cFirst);
+                                } else {
+                                    cFirst.remove("NULL");
+                                    first.addAll(cFirst);
+                                }
+                            } else {
+                                // 如果不包含空串，就不需要继续计算了
+                                first.addAll(cFirst);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return first;
+        }
+    }
+```
+
+然后就可以很轻松地计算FIRST集：
+
+```java
+ // 计算FIRST集合
+private void calculateFIRST(){
+
+	FIRST = new HashMap<>();
+
+	// 终结符号的FIRST集合就是它本身
+	for (String value : endChars){
+		Set<String> first = new HashSet<>();
+         first.add(value);
+         FIRST.put(value, first);
+	}
+
+	// 对于每个非终结符
+	for (String value : nonEndChars){
+         FIRST.put(value, FIRSTx(value));
+	}
+
+}
+```
+
+#### 计算FOLLOW集合
+
+对于计算FOLLOW集合，按照以下的处理方式进行计算：
+
+- 对于开始符号S，先将界符加入到其FOLLOW集中。
+- 如果某个符号是一个句型的最右符号，就把界符加入到其FOLLOW集。
+- 对于`A->aBp`，如果`p`不能推导出空串，就将`FIRST(p)`加入到B的FOLLOW集。
+- 如果`p`能推导出空串，就将`FIRST(p)-空串`和`FOLLOW(A)`加入到B的FOLLOW集。
+
+从以上处理方式也看出来，需要递归地处理：
+
+```java
+// 递归地计算非终结符的FOLLOW集合
+    private Set<String> FOLLOWx(String x){
+        // 在产生式中搜索所有非终结符出现的位置
+        for (String character : nonEndChars){
+            String[] rightItems = P.get(character);
+
+            for (String item : rightItems){
+                // 获取符号列表
+                List<String> cList = disassemble(item);
+
+                // 接下来，搜索当前查找的非终结符的位置
+                for (int i=0; i<cList.size(); i++){
+                    String nonEndChar = cList.get(i);
+                    if (nonEndChar.equals(x)){
+                        // 判断是否处于最右的位置
+                        if (i < cList.size() - 1){
+                            // 如果没在最右边的位置
+                            // 下面循环判断后一个符号是否是非终结符
+                            for (int j=i+1; j<cList.size(); j++){
+                                String nextChar = cList.get(j);
+                                if (nonEndChars.contains(nextChar)){
+                                    // 如果是非终结符，查看其FIRST集是否包含空串
+                                    Set<String> nextFirst = FIRST.get(nextChar);
+                                    // 如果包含空串，并且此时这个符号是最后一个符号
+                                    // 就要将其FIRST除去空串的集合加入FOLLOW集，且左部的FOLLOW集加入FOLLOW集
+                                    if (nextFirst.contains("NULL")){
+                                        // 判断是否是最后一个符号
+                                        if (j == cList.size() - 1){
+                                            Set<String> leftFOLLOW = FOLLOWx(character);
+                                            Set<String> nextFirstExceptNULL = new HashSet<>(nextFirst);
+                                            nextFirstExceptNULL.remove("NULL");
+                                            addCharsToFOLLOW(leftFOLLOW, x);
+                                            addCharsToFOLLOW(nextFirstExceptNULL, x);
+                                        } else{
+                                            Set<String> nextFirstExceptNULL = new HashSet<>(nextFirst);
+                                            nextFirstExceptNULL.remove("NULL");
+                                            addCharsToFOLLOW(nextFirstExceptNULL, x);
+                                        }
+                                    } else{
+                                        // 如果不包含空串加入FIRST之后跳出循环
+                                        addCharsToFOLLOW(nextFirst, x);
+                                        break;
+                                    }
+                                } else{
+                                    // 如果不是非终结符，把此符号加入到当前查找的非终结符的FOLLOW集中
+                                    addCharToFOLLOW(nextChar, nonEndChar);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return FOLLOW.get(x);
+    }
+```
+
+计算FOLLOW集的方法如下：
+
+```java
+  // 计算FOLLOW集合
+    private void calculateFOLLOW(){
+
+        FOLLOW = new HashMap<>();
+        // 首先在开始符号的FOLLOW集中加入界符
+        Set<String> startFollow = new HashSet<>();
+        startFollow.add("#");
+        FOLLOW.put(start, startFollow);
+
+        for (String character : nonEndChars) {
+            String[] rightItems = P.get(character);
+            for (String item : rightItems) {
+                // 获取符号列表
+                List<String> cList = disassemble(item);
+                String rightChar = cList.get(cList.size() - 1);
+                // 如果最右的符号是非终结符，就要把界符加入到其FOLLOW集
+                if (nonEndChars.contains(rightChar)) {
+                    addCharToFOLLOW("#", rightChar);
+                }
+            }
+        }
+
+        System.out.println("FOLLOW集为: ");
+
+        for (String character : nonEndChars){
+            Set<String> follow = FOLLOWx(character);
+            System.out.println("FOLLOW(" + character + "): { ");
+            for (String value : follow){
+                System.out.print(value + " ");
+            }
+            System.out.println(" }");
+        }
+
+    }
+```
+
+#### 计算SELECT集合
+
+- 对于产生式`A->a`，如果符号串`a`能推出空串，`SELECT(A) = FIRST(a)-空串`并上`FOLLOW(A)`。
+
+- 如果不能推导出，那么`SELECT(A) = FIRST(a)`。
+
+```java
+// 计算SELECT集
+    private void calculateSELECT(){
+
+        // 为true的话，说明右部可以推导出空串
+        boolean situation;
+        SELECT = new HashMap<>();
+
+        // 遍历产生式
+        for (Map.Entry<String, String[]> entry : P.entrySet()){
+            String leftItem = entry.getKey();
+            String[] rightItems = entry.getValue();
+
+            // 遍历右部
+            for (String item : rightItems){
+                situation = false;
+                // 分解串为符号
+                List<String> characters = disassemble(item);
+
+                Set<String> first = new HashSet<>();
+
+                // 计算串的FIRST集
+                for (int i=0; i<characters.size(); i++){
+                    String character = characters.get(i);
+                    Set<String> currentFIRST = FIRST.get(character);
+                    if (currentFIRST.contains("NULL")){
+                        // 如果最后一个符号也能推导出空串，说明右部可以推导出空串
+                        if (i == characters.size()-1){
+                            situation = true;
+                            first.addAll(FIRST.get(character));
+                            first.remove("NULL");
+                        } else{
+                            first.addAll(FIRST.get(character));
+                            first.remove("NULL");
+                        }
+                    } else{
+                        first.addAll(FIRST.get(character));
+                        break;
+                    }
+                }
+
+                String p = leftItem + " -> " + item;
+
+                if (situation){
+                    Set<String> follow = FOLLOW.get(leftItem);
+                    first.addAll(follow);
+                    SELECT.put(p, first);
+                } else{
+                    SELECT.put(p, first);
+                }
+            }
+        }
+    }
+```
+
+![](./img/1.png)
+
+![](./img/2.png)
+
+#### 判断是否是LL1文法
+
+只要保证任意两条产生式的可选集不相交就说明是LL1文法。
+
+```java
+  // 判断是否是LL1文法
+    public boolean isLL1(){
+
+        boolean isIntersect = false;
+        List<Set<String>> cList = new ArrayList<>(SELECT.values());
+
+        outer:for (int i=0; i<cList.size(); i++){
+            Set<String> set1 = cList.get(i);
+            for (int j=i+1; j<cList.size(); j++){
+                Set<String> set2 = cList.get(j);
+                if (set1.retainAll(set2)){
+                    break outer;
+                } else{
+                    set1 = cList.get(i);
+                }
+            }
+        }
+
+        return isIntersect;
+    }
+```
+
+完整代码见`LL1Analysis文件`。
---
+++ src/LL1Analysis.java
@@ -0,0 +1,481 @@
+import java.util.*;
+
+/**
+ * @Description 分析判定一个文法是不是LL1文法
+ * @Author Jianlong
+ * @Date 2020-04-27 上午 10:59
+ */
+public class LL1Analysis {
+
+    // 存储文法的映射（产生式规则集合）
+    private Map<String, String[]> P;
+    // 文法的开始符号
+    private String start;
+    // 非终结符号集
+    private Set<String> nonEndChars;
+    // 终结符号集
+    private Set<String> endChars;
+
+    // FIRST集合
+    private Map<String, Set<String>> FIRST;
+    // FOLLOW集合
+    private Map<String, Set<String>> FOLLOW;
+    // SELECT集合
+    private Map<String, Set<String>> SELECT;
+
+    public LL1Analysis(){
+
+        // 非终结符集合
+        String[] Vn = {"E", "E\'", "T", "T\'", "F"};
+        // 终结符集合
+        // 这里定义空串为 NULL
+        String[] Vt = {"+", "NULL", "*", "(", ")", "i"};
+        // 预定义产生式规则集合P
+        String[] P = {
+                "E->TE\'",
+                "E\'->+TE\'|NULL",
+                "T->FT\'",
+                "T\'->*FT\'|NULL",
+                "F->(E)|i"
+        };
+        // 开始符号
+        String S = "E";
+
+        storeGrammar(Vn, Vt, P, S);
+        preProcess();
+    }
+
+    public LL1Analysis(String[] Vn, String[] Vt, String[] P, String start){
+        storeGrammar(Vn, Vt, P, start);
+        preProcess();
+    }
+
+    // 存储文法
+    private void storeGrammar(String[] Vn, String[] Vt, String[] P, String start){
+
+        this.nonEndChars = new HashSet<>();
+        this.endChars = new HashSet<>();
+        this.start = start;
+        this.P = new HashMap<>();
+
+        try{
+            // 存储产生式
+            for (String value : P) {
+                // 分割左部、右部
+                String[] split1 = value.split("->");
+                // 分割右部的不同产生式
+                String[] split2 = split1[1].split("\\|");
+                this.P.put(split1[0], split2);
+            }
+
+            // 存储符号集
+            Collections.addAll(nonEndChars, Vn);
+            Collections.addAll(endChars, Vt);
+        } catch (Exception e){
+            System.out.println("输入文法有错误!");
+            e.printStackTrace();
+        }
+    }
+
+    // 打印文法
+    public void printGrammar(){
+
+        System.out.println("此文法的非终结符号集为: ");
+        System.out.print("{ ");
+        for (String value : nonEndChars){
+            System.out.print(value + " ");
+        }
+        System.out.print("}");
+
+        System.out.println();
+
+        System.out.println("此文法的终结符号集为: ");
+        System.out.print("{ ");
+        for (String value : endChars){
+            System.out.print(value + " ");
+        }
+        System.out.print("}");
+
+        System.out.println();
+
+        System.out.println("此文法的产生式规则集为: ");
+        System.out.println("{ ");
+        // 需要遍历map
+        for (Map.Entry<String, String[]> entry : P.entrySet()){
+            String leftItem = entry.getKey();
+            String[] rightItems = entry.getValue();
+            System.out.print("  " + leftItem + " -> ");
+            for (int i=0; i<rightItems.length; i++){
+                if (i == rightItems.length - 1){
+                    System.out.print(rightItems[i]);
+                } else {
+                    System.out.print(rightItems[i] + " | ");
+                }
+            }
+            System.out.println();
+        }
+        System.out.print("}");
+
+        System.out.println();
+
+        System.out.println("此文法的开始符号为: ");
+        System.out.println(start);
+    }
+
+    // 预处理
+    private void preProcess(){
+        calculateFIRST();
+        calculateFOLLOW();
+        calculateSELECT();
+    }
+
+    // 计算FIRST集合
+    private void calculateFIRST(){
+
+        FIRST = new HashMap<>();
+
+        // 终结符号的FIRST集合就是它本身
+        for (String value : endChars){
+            Set<String> first = new HashSet<>();
+            first.add(value);
+            FIRST.put(value, first);
+        }
+
+        // 对于每个非终结符
+        for (String value : nonEndChars){
+            FIRST.put(value, FIRSTx(value));
+        }
+
+    }
+
+    // 将字符串中的符号拆开
+    private List<String> disassemble(String value){
+        char c;
+        List<String> cList = new ArrayList<>();
+
+        // 将右部分解为一个个的符号
+        for (int i=0; i<value.length(); i++){
+            if (value.equals("NULL")){
+                cList.add(value);
+                break;
+            }
+            if (i+1<value.length() && value.charAt(i+1) == '\''){
+                cList.add(value.substring(i, i+2));
+                i+=1;
+            } else {
+                cList.add(value.substring(i, i+1));
+            }
+        }
+
+        return cList;
+    }
+
+    // 递归地计算某符号的FIRST
+    private Set<String> FIRSTx(String x){
+
+        Set<String> first = new HashSet<>();
+
+        // 如果x是终结符，FIRST集就是它本身
+        // 这里，空串也包含在终结符号里
+        if (endChars.contains(x)){
+            first.add(x);
+            return first;
+        } else {
+            // 否则，就等于其右部各个符号的FIRST集相加
+            String[] rightItems = P.get(x);
+            // 遍历每一个右部
+            for (String value : rightItems){
+                List<String> cList = disassemble(value);
+
+                // 如果右部的产生式是单个符号的，就直接加入到first集合
+                if (cList.size() == 1){
+                    first.addAll(FIRSTx(cList.get(0)));
+                } else {
+                    // 如果右部是多个符号，就看串首符号
+                    for (int i=0; i<cList.size(); i++){
+                        String character = cList.get(i);
+                        // 如果是终结符，直接把它加入first集合
+                        if (endChars.contains(character)){
+                            first.add(character);
+                            break;
+                        } else {
+                            // 如果是非终结符，就计算它的FIRST集
+                            Set<String> cFirst = FIRSTx(character);
+                            // 如果包含有空串，就要继续计算后一个符号的FIRST集
+                            if (cFirst.contains("NULL")){
+                                // 如果此时是最后一个符号了
+                                // 就要把空串页加入进first集合
+                                if (i == cList.size() - 1){
+                                    first.addAll(cFirst);
+                                } else {
+                                    cFirst.remove("NULL");
+                                    first.addAll(cFirst);
+                                }
+                            } else {
+                                // 如果不包含空串，就不需要继续计算了
+                                first.addAll(cFirst);
+                                break;
+                            }
+                        }
+                    }
+                }
+            }
+            return first;
+        }
+    }
+
+    private Map<String, Set<String>> getFIRST(){
+        return this.FIRST;
+    }
+
+    // 将某个符号加入到某个非终结符的FOLLOW集合中
+    private void addCharToFOLLOW(String character, String nonEndChar){
+        Set<String> nonEndCharFollow = FOLLOW.get(nonEndChar);
+        // 加入前先判断有没有在映射中了
+        if (nonEndCharFollow != null){
+            nonEndCharFollow.add(character);
+        } else{
+            nonEndCharFollow = new HashSet<>();
+            nonEndCharFollow.add(character);
+            FOLLOW.put(nonEndChar, nonEndCharFollow);
+        }
+    }
+
+    // 将某个符号集加入到某个非终结符的FOLLOW集合中
+    private void addCharsToFOLLOW(Set<String> characterSet, String nonEndChar){
+        Set<String> nonEndCharFollow = FOLLOW.get(nonEndChar);
+        // 加入前先判断有没有在映射中了
+        if (nonEndCharFollow != null){
+            nonEndCharFollow.addAll(characterSet);
+        } else{
+            nonEndCharFollow = new HashSet<>();
+            nonEndCharFollow.addAll(characterSet);
+            FOLLOW.put(nonEndChar, nonEndCharFollow);
+        }
+    }
+
+    // 计算FOLLOW集合
+    private void calculateFOLLOW(){
+
+        FOLLOW = new HashMap<>();
+        // 首先在开始符号的FOLLOW集中加入界符
+        Set<String> startFollow = new HashSet<>();
+        startFollow.add("#");
+        FOLLOW.put(start, startFollow);
+
+        for (String character : nonEndChars) {
+            String[] rightItems = P.get(character);
+            for (String item : rightItems) {
+                // 获取符号列表
+                List<String> cList = disassemble(item);
+                String rightChar = cList.get(cList.size() - 1);
+                // 如果最右的符号是非终结符，就要把界符加入到其FOLLOW集
+                if (nonEndChars.contains(rightChar)) {
+                    addCharToFOLLOW("#", rightChar);
+                }
+            }
+        }
+
+        System.out.println("FOLLOW集为: ");
+
+        for (String character : nonEndChars){
+            Set<String> follow = FOLLOWx(character);
+            System.out.println("FOLLOW(" + character + "): { ");
+            for (String value : follow){
+                System.out.print(value + " ");
+            }
+            System.out.println(" }");
+        }
+
+    }
+
+    // 递归地计算非终结符的FOLLOW集合
+    private Set<String> FOLLOWx(String x){
+        // 在产生式中搜索所有非终结符出现的位置
+        for (String character : nonEndChars){
+            String[] rightItems = P.get(character);
+
+            for (String item : rightItems){
+                // 获取符号列表
+                List<String> cList = disassemble(item);
+
+                // 接下来，搜索当前查找的非终结符的位置
+                for (int i=0; i<cList.size(); i++){
+                    String nonEndChar = cList.get(i);
+                    if (nonEndChar.equals(x)){
+                        // 判断是否处于最右的位置
+                        if (i < cList.size() - 1){
+                            // 如果没在最右边的位置
+                            // 下面循环判断后一个符号是否是非终结符
+                            for (int j=i+1; j<cList.size(); j++){
+                                String nextChar = cList.get(j);
+                                if (nonEndChars.contains(nextChar)){
+                                    // 如果是非终结符，查看其FIRST集是否包含空串
+                                    Set<String> nextFirst = FIRST.get(nextChar);
+                                    // 如果包含空串，并且此时这个符号是最后一个符号
+                                    // 就要将其FIRST除去空串的集合加入FOLLOW集，且左部的FOLLOW集加入FOLLOW集
+                                    if (nextFirst.contains("NULL")){
+                                        // 判断是否是最后一个符号
+                                        if (j == cList.size() - 1){
+                                            Set<String> leftFOLLOW = FOLLOWx(character);
+                                            Set<String> nextFirstExceptNULL = new HashSet<>(nextFirst);
+                                            nextFirstExceptNULL.remove("NULL");
+                                            addCharsToFOLLOW(leftFOLLOW, x);
+                                            addCharsToFOLLOW(nextFirstExceptNULL, x);
+                                        } else{
+                                            Set<String> nextFirstExceptNULL = new HashSet<>(nextFirst);
+                                            nextFirstExceptNULL.remove("NULL");
+                                            addCharsToFOLLOW(nextFirstExceptNULL, x);
+                                        }
+                                    } else{
+                                        // 如果不包含空串加入FIRST之后跳出循环
+                                        addCharsToFOLLOW(nextFirst, x);
+                                        break;
+                                    }
+                                } else{
+                                    // 如果不是非终结符，把此符号加入到当前查找的非终结符的FOLLOW集中
+                                    addCharToFOLLOW(nextChar, nonEndChar);
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return FOLLOW.get(x);
+    }
+
+    public Map<String, Set<String>> getFOLLOW(){
+        return FOLLOW;
+    }
+
+    // 计算SELECT集
+    private void calculateSELECT(){
+
+        // 为true的话，说明右部可以推导出空串
+        boolean situation;
+        SELECT = new HashMap<>();
+
+        // 遍历产生式
+        for (Map.Entry<String, String[]> entry : P.entrySet()){
+            String leftItem = entry.getKey();
+            String[] rightItems = entry.getValue();
+
+            // 遍历右部
+            for (String item : rightItems){
+                situation = false;
+                // 分解串为符号
+                List<String> characters = disassemble(item);
+
+                Set<String> first = new HashSet<>();
+
+                // 计算串的FIRST集
+                for (int i=0; i<characters.size(); i++){
+                    String character = characters.get(i);
+                    Set<String> currentFIRST = FIRST.get(character);
+                    if (currentFIRST.contains("NULL")){
+                        // 如果最后一个符号也能推导出空串，说明右部可以推导出空串
+                        if (i == characters.size()-1){
+                            situation = true;
+                            first.addAll(FIRST.get(character));
+                            first.remove("NULL");
+                        } else{
+                            first.addAll(FIRST.get(character));
+                            first.remove("NULL");
+                        }
+                    } else{
+                        first.addAll(FIRST.get(character));
+                        break;
+                    }
+                }
+
+                String p = leftItem + " -> " + item;
+
+                if (situation){
+                    Set<String> follow = FOLLOW.get(leftItem);
+                    first.addAll(follow);
+                    SELECT.put(p, first);
+                } else{
+                    SELECT.put(p, first);
+                }
+            }
+        }
+    }
+
+    public Map<String, Set<String>> getSELECT(){
+        return SELECT;
+    }
+
+    // 判断是否是LL1文法
+    public boolean isLL1(){
+
+        boolean isIntersect = false;
+        List<Set<String>> cList = new ArrayList<>(SELECT.values());
+
+        outer:for (int i=0; i<cList.size(); i++){
+            Set<String> set1 = cList.get(i);
+            for (int j=i+1; j<cList.size(); j++){
+                Set<String> set2 = cList.get(j);
+                if (set1.retainAll(set2)){
+                    break outer;
+                } else{
+                    set1 = cList.get(i);
+                }
+            }
+        }
+
+        return isIntersect;
+    }
+
+    public static void main(String[] args) {
+        // 非终结符集合
+        String[] Vn = {"S", "S\'", "B", "A"};
+        // 终结符集合
+        // 这里定义空串为 NULL
+        String[] Vt = {"a", "b", "e", "NULL"};
+        // 预定义产生式规则集合P
+        String[] P = {
+                "S->aBS\'",
+                "S\'->bBS\'|NULL",
+                "B->Ab|e",
+                "A->a|NULL"
+        };
+        // 开始符号
+        String S = "S";
+
+        LL1Analysis myLL1 = new LL1Analysis(Vn, Vt, P, S);
+        // 打印出文法
+        myLL1.printGrammar();
+
+        System.out.println();
+        System.out.println("FIRST集为: ");
+
+        Map<String, Set<String>> FIRST = myLL1.getFIRST();
+        for (Map.Entry<String, Set<String>> entry : FIRST.entrySet()){
+            System.out.print("FIRST(" + entry.getKey() + "): { ");
+            for (String value : entry.getValue()){
+                System.out.print(value + " ");
+            }
+            System.out.println("}");
+        }
+
+        System.out.println();
+
+        System.out.println();
+        System.out.println("SELECT集为: ");
+
+        Map<String, Set<String>> SELECT = myLL1.getSELECT();
+        for (Map.Entry<String, Set<String>> entry : SELECT.entrySet()){
+            System.out.print("SELECT(" + entry.getKey() + "): { ");
+            for (String value : entry.getValue()){
+                System.out.print(value + " ");
+            }
+            System.out.println("}");
+        }
+
+        System.out.println();
+        System.out.println("该文法是LL1文法吗？ " + myLL1.isLL1());
+    }
+}
